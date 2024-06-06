9ac9dbc5e85c1663b008e194f9983c5e7b2812ff
Ajit raut <ajitraut9561@gmail.com>
2024-06-06 21:54:09 +0530

    Avg Array Add

---
+++ ARRAYS/AvgArray.js
@@ -0,0 +1,13 @@
+// Average of all the elements in the array
+
+const AvgArray = (arr) => {
+  let avg = 0;
+  let sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = sum / arr.length;
+  }
+  return avg;
+};
+let arr = [1, 2, 1, 1, 5, 1];
+console.log(AvgArray(arr));
--- STRINGS/StringEvqivalent.js
+++ STRINGS/StringEvqivalent.js
@@ -1,10 +1,11 @@
-// Check The Both Strings Are Equivalent Or Not
+// Check The Both Strings Are Equivalent Or Not return True Or false.
 
-var arrayStringsAreEqual = function(word1, word2) {
-    word1=word1.join("")
-    word2=word2.join("")
+var arrayStringsAreEqual = function (word1, word2) {
+  word1 = word1.join("");
+  word2 = word2.join("");
 
-    return word1===word2
+  return word1 === word2;
 };
-let word1 = ["ab", "c"], word2 = ["ad", "bc"];
-console.log(arrayStringsAreEqual(word1,word2));
+let word1 = ["ab", "c"],
+  word2 = ["ad", "bc"];
+console.log(arrayStringsAreEqual(word1, word2));

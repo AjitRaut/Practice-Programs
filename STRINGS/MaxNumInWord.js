73e83c82fdd646586c940421b3dc8ecb0a282267
Ajit raut <ajitraut9561@gmail.com>
2024-06-07 20:37:05 +0530

    Palindrome Number

--- STRINGS/MaxNumInWord.js
+++
@@ -1,7 +0,0 @@
-// Find Maximum Numbers From Given Sentence.
-
-var mostWordsFound = function(sentences) {
-    const mosword=sentences.map(item => item.split(' ')).sort((a,b) => b.length - a.length)[0].length
-    return mosword
-};
-console.log(mostWordsFound(sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]))

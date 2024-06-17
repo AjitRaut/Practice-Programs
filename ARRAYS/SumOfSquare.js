35c73bbc70ccdc14bd6d731cb6aa3f550b59196c
Ajit raut <ajitraut9561@gmail.com>
2024-06-17 16:28:59 +0530

    Sum Of Square Numbers

---
+++ ARRAYS/SumOfSquare.js
@@ -0,0 +1,21 @@
+// Sum Of Square.
+var judgeSquareSum = function(c) {
+    let i=0;
+    let sum;
+    let j=Math.ceil(Math.sqrt(c));  //Here we get square root od that number
+    // console.log(j)
+    while(i<=j){                    
+        sum=i*i+j*j;
+        // console.log(sum)
+        if(sum<c){
+            i++
+        }else if(sum>c){
+            j--
+        }else{
+            return true;
+        }
+    }
+    return false
+};
+console.log(judgeSquareSum(5))
+console.log(judgeSquareSum(4))
